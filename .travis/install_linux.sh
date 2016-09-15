15683891b3a246158694b914b6f9443a15595eee
Leszek Grzanka <leszek.grzanka@gmail.com>
2016-09-15 20:49:25 +0200

    py35 test

--- .travis/install_linux.sh
+++ .travis/install_linux.sh
@@ -11,18 +11,13 @@ set -o pipefail # Return value of a pipeline as the value of the last command to
 # check ubuntu version
 
 # instruction valid for Ubuntu 14.04
-if [[ $TOXENV == py27* ]] || [[ $TOXENV == py35* ]];
-then
-    sudo apt-get -qq update
-    # pkg-config libwxgtk2.8-dev freeglut3-dev
-    # http://tutorialforlinux.com/2014/11/09/how-to-install-wxpython-python-3-on-ubuntu-14-04-trusty-lts-32-64bit-easy-guide/
-    # http://stackoverflow.com/questions/27240143/installing-wxpython-on-ubuntu-14-04
-    sudo apt-get install -y libblas-dev liblapack-dev gfortran python-wxgtk2.8 python-tk libfreetype6-dev libwxgtk2.8-dev libwxgtk-media2.8-dev libwxgtk-media2.8-0 libwxgtk2.8-0  libwxgtk3.0-dev
-    sudo apt-get install -y make gcc libgtkgl2.0-dev libgstreamer* libwebkit-dev
-    sudo apt-get install -y pkg-config libwxgtk2.8-dev freeglut3-dev
-    pip install -U --trusted-host wxpython.org --pre -f http://wxpython.org/Phoenix/snapshot-builds/ wxPython_Phoenix==3.0.3.dev2487+3b86464
-fi
+sudo apt-get -qq update
+# pkg-config libwxgtk2.8-dev freeglut3-dev
+# http://tutorialforlinux.com/2014/11/09/how-to-install-wxpython-python-3-on-ubuntu-14-04-trusty-lts-32-64bit-easy-guide/
+# http://stackoverflow.com/questions/27240143/installing-wxpython-on-ubuntu-14-04
+sudo apt-get install -y libblas-dev liblapack-dev gfortran python-wxgtk2.8 python-tk libfreetype6-dev libwxgtk2.8-dev libwxgtk-media2.8-dev libwxgtk-media2.8-0 libwxgtk2.8-0  libwxgtk3.0-dev make gcc libgtkgl2.0-dev libgstreamer* libwebkit-dev pkg-config freeglut3-dev
+pip install -U --trusted-host wxpython.org --pre -f http://wxpython.org/Phoenix/snapshot-builds/ wxPython_Phoenix==3.0.3.dev2487+3b86464
 
-pip install --upgrade virtualenv pip setuptools tox wheel
+pip install --upgrade pip setuptools wheel
 
 pip install -r requirements.txt
