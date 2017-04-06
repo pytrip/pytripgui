0c695fd3f1de4044d53e483a1b771f32a143d447
Niels Bassler <niels.bassler@fysik.su.se>
2017-04-06 14:36:48 +0200

    revert main

--- pytripgui/main.py
+++ pytripgui/main.py
@@ -39,11 +39,13 @@ from pytripgui.util import get_resource_path
 from pytripgui import util
 
 if getattr(sys, 'frozen', False):
+    from wx.lib.pubsub import setuparg1  # noqa
     from wx.lib.pubsub import pub
 else:
     try:
         from wx.lib.pubsub import Publisher as pub
     except:
+        from wx.lib.pubsub import setuparg1  # noqa
         from wx.lib.pubsub import pub
 
 logger = logging.getLogger(__name__)
