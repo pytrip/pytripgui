e1f3e165527cace8c85adec2211e243518bb851c
Leszek Grzanka <leszek.grzanka@gmail.com>
2018-04-19 23:43:36 +0200

    automatic file generation script added

--- makeui.sh
+++ makeui.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+
+set -x # Print command traces before executing command
+set -e # Exit immediately if a simple command exits with a non-zero status.
+
 pyuic5 pytripgui/view/main_window.ui > pytripgui/view/main_window.py
 pyuic5 pytripgui/view/trip_config.ui > pytripgui/view/trip_config.py
 
