9f2b78eba193dfdaf14b603c324e443d884c3721
Leszek Grzanka <leszek.grzanka@gmail.com>
2018-04-20 00:54:12 +0200

    new tests

--- tests/test_pytripgui.py
+++ tests/test_pytripgui.py
@@ -1,15 +1,13 @@
-import unittest
+import pytest
+from pytestqt.qt_compat import qt_api
 
-from pytripgui.main import main as run_main
+from pytripgui.main import AppWindow
 
+def test_basics(qtbot):
+    assert qt_api.QApplication.instance() is not None
+    widget = AppWindow()
+    qtbot.addWidget(widget)
+    widget.show()
 
-class TestStart(unittest.TestCase):
-    def test_check(self):
-        try:
-            run_main()
-        except SystemExit as e:
-            self.assertTrue(True)
-
-
-if __name__ == '__main__':
-    unittest.main()
+    assert widget.isVisible()
+    assert widget.windowTitle() == 'PyTRiPGUI'
