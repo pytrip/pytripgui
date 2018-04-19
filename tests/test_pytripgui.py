1bf4415cf2d3cbe7e47386bf503f2fd149c03780
Leszek Grzanka <leszek.grzanka@gmail.com>
2018-04-19 23:46:07 +0200

    test improved

--- tests/test_pytripgui.py
+++ tests/test_pytripgui.py
@@ -1,12 +1,12 @@
 import unittest
 
-from pytripgui.main import main
+from pytripgui.main import main as run_main
 
 
 class TestStart(unittest.TestCase):
     def test_check(self):
         try:
-            main.main()
+            run_main()
         except SystemExit as e:
             self.assertEqual(e.code, 'Unable to access the X Display, is $DISPLAY set properly?')
 
