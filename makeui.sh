bee526932ce14778f0b523a621f36f30177054ae
Leszek Grzanka <leszek.grzanka@gmail.com>
2018-04-20 17:03:00 +0200

    testing new workaround

--- makeui.sh
+++ makeui.sh
@@ -5,4 +5,3 @@ set -e # Exit immediately if a simple command exits with a non-zero status.
 
 pyuic5 pytripgui/view/main_window.ui > pytripgui/view/main_window.py
 pyuic5 pytripgui/view/trip_config.ui > pytripgui/view/trip_config.py
-
