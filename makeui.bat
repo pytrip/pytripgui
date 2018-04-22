c087e1368cad548f1aac90e5826737b5c8482c45
Leszek Grzanka <leszek.grzanka@gmail.com>
2018-04-22 20:28:59 +0200

    create empty directory for generated files

--- makeui.bat
+++ makeui.bat
@@ -1,3 +1,4 @@
+mkdir -p pytripgui\view\gen\
 pyuic5 pytripgui\view\main_window.ui -o pytripgui\view\gen\main_window.py
 pyuic5 pytripgui\view\trip_config.ui -o pytripgui\view\gen\trip_config.py
 
--- makeui.sh
+++ makeui.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-=======
 set -x # Print command traces before executing command
 set -e # Exit immediately if a simple command exits with a non-zero status.
 
+mkdir -p pytripgui/view/gen/
 pyuic5 pytripgui/view/main_window.ui -o pytripgui/view/gen/main_window.py
 pyuic5 pytripgui/view/trip_config.ui -o pytripgui/view/gen/trip_config.py
