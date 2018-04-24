8caefdee97bb60b8fb58ceaf651466e612b2f72e
Niels Bassler <niels.bassler@gmail.com>
2018-04-24 23:09:31 +0200

    update makeui.sh

--- makeui.sh
+++ makeui.sh
@@ -5,3 +5,5 @@ set -e # Exit immediately if a simple command exits with a non-zero status.
 mkdir -p pytripgui/view/gen/
 pyuic5 pytripgui/view/main_window.ui -o pytripgui/view/gen/main_window.py
 pyuic5 pytripgui/view/trip_config.ui -o pytripgui/view/gen/trip_config.py
+pyuic5 pytripgui/view/field.ui -o pytripgui/view/gen/field.py
+pyuic5 pytripgui/view/plan.ui -o pytripgui/view/gen/plan.py
