76a71c844d299d2e53910d478ab40936ab0dcdbd
Leszek Grzanka <leszek.grzanka@gmail.com>
2018-05-02 12:46:02 +0200

    unneeded file removed

--- makeui.bat
+++
@@ -1,5 +0,0 @@
-mkdir -p pytripgui\view\gen\
-pyuic5 pytripgui\view\main_window.ui -o pytripgui\view\gen\main_window.py
-pyuic5 pytripgui\view\trip_config.ui -o pytripgui\view\gen\trip_config.py
-pyuic5 pytripgui\view\field.ui -o pytripgui\view\gen\field.py
-pyuic5 pytripgui\view\plan.ui -o pytripgui\view\gen\plan.py
