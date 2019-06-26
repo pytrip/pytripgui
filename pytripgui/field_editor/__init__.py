2de0b22ad719fe434da3efaa90046256c01f1e42
Lukasz Jelen <lukasz.jelen@randlab.pl>
2019-06-26 14:39:41 +0200

    repairs flake F401 imported but unused

--- pytripgui/field_editor/__init__.py
+++ pytripgui/field_editor/__init__.py
@@ -1,2 +1,7 @@
 from pytripgui.field_editor.field_view import FieldQtView
 from pytripgui.field_editor.field_cont import FieldController
+
+# from https://docs.python.org/3/tutorial/modules.html
+# if a package's __init__.py code defines a list named __all__,
+# it is taken to be the list of module names that should be imported when from package import * is encountered.
+__all__ = ['FieldQtView', 'FieldController']
