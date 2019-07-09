aad6e17b0834342c7c3edcbc0e3683e1fc14a908
Lucas Jelen <lukasz.jelen@randlab.pl>
2019-07-09 19:50:57 +0200

    Adding _init_ code

---
+++ pytripgui/config_vc/__init__.py
@@ -0,0 +1,7 @@
+from pytripgui.config_vc.config_view import ConfigQtView
+from pytripgui.config_vc.config_cont import ConfigController
+
+# from https://docs.python.org/3/tutorial/modules.html
+# if a package's __init__.py code defines a list named __all__,
+# it is taken to be the list of module names that should be imported when from package import * is encountered.
+__all__ = ['ConfigQtView', 'ConfigController']
