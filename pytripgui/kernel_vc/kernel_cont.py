dfe694e0f56508a0cce2e28460813f7c97e3e114
ljelen <47323191+ljelen@users.noreply.github.com>
2019-08-25 23:17:38 +0200

    Enhancement/219 add kernel vc (#220)
    * trip execute plan first attempt
    
    * fix: file selection
    
    * Add initial code for kernel_vc
    
    * add logic controller to kernel_vc
    
    * Add: logic to remove and new kernel buttons
    Connect kernel_vc with app
    Enhancement: while editing kernel name, name in combobox is also changing
    
    * Add persistent save of kernel data
    
    * Sorting projectile symbols

---
+++ pytripgui/kernel_vc/kernel_cont.py
@@ -0,0 +1,123 @@
+from pytrip.tripexecuter import KernelModel
+from pytrip.tripexecuter import Projectile
+
+import logging
+logger = logging.getLogger(__name__)
+
+
+class KernelController(object):
+    def __init__(self, model, view):
+        self.kernels = model
+        self.last_kernel_index = None
+        self.view = view
+        self.user_clicked_save = False
+        self._setup_ok_and_cancel_buttons_callbacks()
+        self.view.set_selected_beam_kernel_callback(self._current_kernel_index_has_changed)
+        self.view.new_beam_kernel_callback(self._new_beam_kernel)
+        self.view.remove_beam_kernel_callback(self._remove_current_beam_kernel)
+
+    def _setup_ok_and_cancel_buttons_callbacks(self):
+        self.view.set_ok_callback(self._save_and_exit)
+        self.view.set_cancel_callback(self._exit)
+
+    def _save_and_exit(self):
+        self.set_model_from_view()
+        self.user_clicked_save = True
+        self.view.exit()
+
+    def _exit(self):
+        self.view.exit()
+
+    def _current_kernel_index_has_changed(self, index):
+        # this mean there is no data on kernel list
+        if index == -1:
+            self._new_beam_kernel()
+
+        # if item was deleted, then this statement is false
+        if index != self.last_kernel_index:
+            # save previous kernel config to VIEW memory
+            if self.last_kernel_index is not None:
+                current_kernel_config = self._visible_kernel_config
+                self.view.replace_kernel_by_index(current_kernel_config, self.last_kernel_index)
+
+        # load new kernel config
+        self.last_kernel_index = index
+        new_kernel_config = self.view.get_selected_kernel()
+        self._visible_kernel_config = new_kernel_config
+
+    def _new_beam_kernel(self):
+        kernel = KernelModel("Kernel")
+        kernel.projectile = Projectile("H")
+        self.view.add_kernel_with_name(kernel, kernel.name)
+        self.view.select_recently_added_kernel()
+
+    def _remove_current_beam_kernel(self):
+        self.view.remove_current_kernel()
+
+    def set_view_from_model(self):
+        # projectile symbols should be setup before setup any kernel
+        sorted_projectile = [y[0] for y in sorted(Projectile._projectile_defaults.items(), key=lambda x: x[1][0])]
+        self.view.setup_all_available_projectile_symbols(sorted_projectile)
+        if not self.kernels:
+            self._new_beam_kernel()
+            return
+
+        self._setup_kernels()
+
+    def _setup_kernels(self):
+        for kernel in self.kernels:
+            self.view.add_kernel_with_name(kernel, kernel.name)
+
+    def set_model_from_view(self):
+        # saves current kernel config to GUI memory
+        self.view.replace_kernel_by_index(self._visible_kernel_config, self.last_kernel_index)
+
+        # read all data from GUI memory
+        kernels = self.view.get_all_kernels()
+        self.kernels.clear()
+        for kernel in kernels:
+            self.kernels.append(kernel)
+
+    @property
+    def _visible_kernel_config(self):
+        kernel = KernelModel()
+        kernel.comment = self.view.comment
+        kernel.projectile = Projectile("H")
+        kernel.projectile.name = self.view.projectile_name
+        kernel.projectile.iupac = self.view.projectile_symbol
+        kernel.projectile.z = self.view.z
+        kernel.projectile.a = self.view.a
+        kernel.ddd_path = self.view.ddd_dir_path
+        kernel.spc_path = self.view.spc_dir_path
+        kernel.sis_path = self.view.sis_path
+        kernel.name = self.view.kernel_name
+        return kernel
+
+    @_visible_kernel_config.getter
+    def _visible_kernel_config(self):
+        kernel = KernelModel()
+        kernel.comment = self.view.comment
+        kernel.projectile = Projectile("H")
+        kernel.projectile.name = self.view.projectile_name
+        kernel.projectile.iupac = self.view.projectile_symbol
+        kernel.projectile.z = self.view.z
+        kernel.projectile.a = self.view.a
+        kernel.ddd_path = self.view.ddd_dir_path
+        kernel.spc_path = self.view.spc_dir_path
+        kernel.sis_path = self.view.sis_path
+        kernel.name = self.view.kernel_name
+        return kernel
+
+    @_visible_kernel_config.setter
+    def _visible_kernel_config(self, kernel):
+        self.view.comment = kernel.comment
+        if kernel.projectile is None:
+            kernel.projectile = Projectile("H")
+        self.view.projectile_name = kernel.projectile.name
+        self.view.projectile_symbol = kernel.projectile.iupac
+        self.view.z = kernel.projectile.z
+        self.view.a = kernel.projectile.a
+        self.view.ddd_dir_path = kernel.ddd_path
+        self.view.spc_dir_path = kernel.spc_path
+        self.view.sis_path = kernel.sis_path
+        self.view.kernel_name = kernel.name
---
+++ pytripgui/kernel_vc/kernel_view.py
@@ -0,0 +1,225 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QFileDialog
+from PyQt5.QtCore import Qt
+
+from pytripgui.view.gen.kernel import Ui_KernelDialog
+import os
+
+import logging
+logger = logging.getLogger(__name__)
+
+
+class KernelQtView(object):
+    """
+    """
+    def __init__(self):
+        self.ui = Ui_KernelDialog()
+        self.dialog = QtWidgets.QDialog()
+
+        self.ui.setupUi(self.dialog)
+        self._setup_internal_callbacks()
+        self._disable_unimplemented()
+
+    def _setup_internal_callbacks(self):
+        self.ui.dddPath_pushButton.clicked.connect(self._browse_ddd_dir)
+        self.ui.spcPath_pushButton.clicked.connect(self._browse_spc_dir)
+        self.ui.sisPath_pushButton.clicked.connect(self._browse_sis_path)
+        self.ui.kernelName_lineEdit.textChanged.connect(self._set_current_kernel_combobox_name)
+
+    def _set_current_kernel_combobox_name(self, name):
+        current_index = self.ui.beamKernel_comboBox.currentIndex()
+        self.ui.beamKernel_comboBox.setItemText(current_index, name)
+
+    def _browse_ddd_dir(self):
+        selected_dir = QFileDialog.getExistingDirectory(
+            self.dialog,
+            "Select DDD directory",
+            self.ddd_dir_path,
+            QFileDialog.DontResolveSymlinks)
+        if selected_dir != "":
+            self.ddd_dir_path = os.path.join(selected_dir, "", "*")
+
+    def _browse_spc_dir(self):
+        selected_dir = QFileDialog.getExistingDirectory(
+            self.dialog,
+            "Select SPC directory",
+            self.spc_dir_path,
+            QFileDialog.DontResolveSymlinks)
+        if selected_dir != "":
+            self.spc_dir_path = os.path.join(selected_dir, "", "*")
+
+    def _browse_sis_path(self):
+        selected_file = QFileDialog.getOpenFileName(
+            self.dialog,
+            "Select sis path",
+            self.sis_path,
+            "(*.sis)")
+        if selected_file[0] != "":
+            self.sis_path = selected_file[0]
+
+    def _disable_unimplemented(self):
+        self.ui.rippleFilter_checkBox.setDisabled(True)
+        self.ui.importBeamKernel_pushButton.setDisabled(True)
+        self.ui.exportBeamKernel_pushButton.setDisabled(True)
+
+    def show(self):
+        self.dialog.show()
+        self.dialog.exec_()
+
+    def exit(self):
+        self.dialog.close()
+
+    def set_ok_callback(self, fun):
+        self.ui.accept_buttonBox.accepted.connect(fun)
+
+    def set_cancel_callback(self, fun):
+        self.ui.accept_buttonBox.rejected.connect(fun)
+
+    def set_selected_beam_kernel_callback(self, fun):
+        self.ui.beamKernel_comboBox.currentIndexChanged.connect(fun)
+
+    def new_beam_kernel_callback(self, fun):
+        self.ui.newBeamKernel_pushButton.clicked.connect(fun)
+
+    def remove_beam_kernel_callback(self, fun):
+        self.ui.removeBeamKernel_pushButton.clicked.connect(fun)
+
+    @property
+    def ddd_dir_path(self):
+        return self.ui.dddPath_lineEdit.text()
+
+    @ddd_dir_path.getter
+    def ddd_dir_path(self):
+        return self.ui.dddPath_lineEdit.text()
+
+    @ddd_dir_path.setter
+    def ddd_dir_path(self, ddd_dir_path):
+        self.ui.dddPath_lineEdit.setText(ddd_dir_path)
+
+    @property
+    def spc_dir_path(self):
+        return self.ui.spcPath_lineEdit.text()
+
+    @spc_dir_path.getter
+    def spc_dir_path(self):
+        return self.ui.spcPath_lineEdit.text()
+
+    @spc_dir_path.setter
+    def spc_dir_path(self, spc_dir_path):
+        self.ui.spcPath_lineEdit.setText(spc_dir_path)
+
+    @property
+    def sis_path(self):
+        return self.ui.sisPath_lineEdit.text()
+
+    @sis_path.getter
+    def sis_path(self):
+        return self.ui.sisPath_lineEdit.text()
+
+    @sis_path.setter
+    def sis_path(self, spc_dir_path):
+        self.ui.sisPath_lineEdit.setText(spc_dir_path)
+
+    @property
+    def comment(self):
+        return self.ui.comment_lineEdit.text()
+
+    @comment.getter
+    def comment(self):
+        return self.ui.comment_lineEdit.text()
+
+    @comment.setter
+    def comment(self, comment):
+        self.ui.comment_lineEdit.setText(comment)
+
+    @property
+    def projectile_name(self):
+        return self.ui.projectileName_lineEdit.text()
+
+    @projectile_name.getter
+    def projectile_name(self):
+        return self.ui.projectileName_lineEdit.text()
+
+    @projectile_name.setter
+    def projectile_name(self, projectile_name):
+        self.ui.projectileName_lineEdit.setText(projectile_name)
+
+    @property
+    def projectile_symbol(self):
+        return self.ui.symbol_comboBox.currentData()
+
+    @projectile_symbol.getter
+    def projectile_symbol(self):
+        return self.ui.symbol_comboBox.currentData()
+
+    @projectile_symbol.setter
+    def projectile_symbol(self, projectile_symbol):
+        index_of_symbol = self.ui.symbol_comboBox.findData(projectile_symbol, Qt.UserRole)
+        if index_of_symbol == -1:
+            raise Exception("Given symbol wasn't found on the list")
+        self.ui.symbol_comboBox.setCurrentIndex(index_of_symbol)
+
+    @property
+    def z(self):
+        return self.ui.z_spinBox.value()
+
+    @z.getter
+    def z(self):
+        return self.ui.z_spinBox.value()
+
+    @z.setter
+    def z(self, z):
+        self.ui.z_spinBox.setValue(z)
+
+    @property
+    def a(self):
+        return self.ui.a_spinBox.value()
+
+    @a.getter
+    def a(self):
+        return self.ui.a_spinBox.value()
+
+    @a.setter
+    def a(self, a):
+        self.ui.a_spinBox.setValue(a)
+
+    @property
+    def kernel_name(self):
+        return self.ui.kernelName_lineEdit.text()
+
+    @kernel_name.getter
+    def kernel_name(self):
+        return self.ui.kernelName_lineEdit.text()
+
+    @kernel_name.setter
+    def kernel_name(self, kernel_name):
+        return self.ui.kernelName_lineEdit.setText(kernel_name)
+
+    def add_kernel_with_name(self, kernel, kernel_name):
+        self.ui.beamKernel_comboBox.addItem(kernel_name, kernel)
+
+    def select_recently_added_kernel(self):
+        kernel_index = self.ui.beamKernel_comboBox.count() - 1
+        self.ui.beamKernel_comboBox.setCurrentIndex(kernel_index)
+
+    def remove_current_kernel(self):
+        kernel_index = self.ui.beamKernel_comboBox.currentIndex()
+        self.ui.beamKernel_comboBox.removeItem(kernel_index)
+
+    def get_all_kernels(self):
+        kernels_count = self.ui.beamKernel_comboBox.count()
+        kernels = list()
+        for i in range(kernels_count):
+            kernel = self.ui.beamKernel_comboBox.itemData(i, Qt.UserRole)
+            kernels.append(kernel)
+        return kernels
+
+    def replace_kernel_by_index(self, kernel, kernel_index):
+        self.ui.beamKernel_comboBox.setItemData(kernel_index, kernel, Qt.UserRole)
+
+    def setup_all_available_projectile_symbols(self, symbols):
+        for symbol in symbols:
+            self.ui.symbol_comboBox.addItem(symbol, symbol)
+
+    def get_selected_kernel(self):
+        return self.ui.beamKernel_comboBox.currentData(Qt.UserRole)
