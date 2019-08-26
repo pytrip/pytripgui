5dbd77064f2a0564e04e9ef84221535f2c459076
Lucas Jelen <lukasz.jelen@randlab.pl>
2019-08-26 21:07:19 +0200

    delete unwanted 'view'

--- pytripgui/view/qt_gui.py
+++ pytripgui/view/qt_gui.py
@@ -5,33 +5,33 @@ import os
 class UiTripConfig(QtWidgets.QDialog):
     def __init__(self):
         super(UiTripConfig, self).__init__()
-        ui_path = os.path.join(os.curdir, 'view', 'trip_config.ui')
+        ui_path = os.path.join(os.curdir, 'trip_config.ui')
         uic.loadUi(ui_path, self)
 
 
 class UiFieldDialog(QtWidgets.QDialog):
     def __init__(self):
         super(UiFieldDialog, self).__init__()
-        ui_path = os.path.join(os.curdir, 'view', 'field.ui')
+        ui_path = os.path.join(os.curdir, 'field.ui')
         uic.loadUi(ui_path, self)
 
 
 class UiKernelDialog(QtWidgets.QDialog):
     def __init__(self):
         super(UiKernelDialog, self).__init__()
-        ui_path = os.path.join(os.curdir, 'view', 'kernel.ui')
+        ui_path = os.path.join(os.curdir, 'kernel.ui')
         uic.loadUi(ui_path, self)
 
 
 class UiPlanDialog(QtWidgets.QDialog):
     def __init__(self):
         super(UiPlanDialog, self).__init__()
-        ui_path = os.path.join(os.curdir, 'view', 'plan.ui')
+        ui_path = os.path.join(os.curdir, 'plan.ui')
         uic.loadUi(ui_path, self)
 
 
 class UiMainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(UiMainWindow, self).__init__()
-        ui_path = os.path.join(os.curdir, 'view', 'main_window.ui')
+        ui_path = os.path.join(os.curdir, 'main_window.ui')
         uic.loadUi(ui_path, self)
