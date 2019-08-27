d9308485cbcb43020d41b293719386d642c1254a
Lucas Jelen <lukasz.jelen@randlab.pl>
2019-08-27 23:33:49 +0200

    naming changes

--- pytripgui/model/let_container.py
+++ pytripgui/model/let_container.py
@@ -8,7 +8,7 @@ class LetContainer:
     def __init__(self):
         self.let_list = []
 
-    def import_let_from_file(self, let_path):
+    def import_from_file(self, let_path):
         logger.debug("Open LETCube {:s}".format(let_path))
         let = pt.LETCube()
         let.read(let_path)
@@ -17,7 +17,7 @@ class LetContainer:
         self.let_list.append(let)
         return let
 
-    def import_let_from_list(self, let_list):
+    def import_from_list(self, let_list):
         for let in let_list:
             self.let_list.append(let)
 
