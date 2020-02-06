768dcae5ec7101ff4aff205b6fbc8e1ab2a15f83
Leszek Grzanka <grzanka@agh.edu.pl>
2020-02-06 16:53:07 +0100

    fixing PEP8 issues

--- pytripgui/__init__.py
+++ pytripgui/__init__.py
@@ -1,12 +1,19 @@
+# find specification of the pytripgui module
+import importlib
+import os
+
+# get location of __init__.py file (the one you see now) in the filesystem
+spec = importlib.util.find_spec('pytripgui')
+init_location = spec.origin
+
+# VERSION should sit next to __init__.py in the directory structure
+version_file = os.path.join(os.path.dirname(init_location), 'VERSION')
+
 try:
-    # find specification of the pytripgui module
-    import importlib
-    import os
-    spec = importlib.util.find_spec('pytripgui')
-    init_location = spec.origin  # location of __init__.py file (the one you see now) in the filesystem
-    version_file = os.path.join(os.path.dirname(init_location), 'VERSION')  # VERSION should sit next to __init__.py in the directory structure
+    # read first line of the file
     with open(version_file, 'r') as f:
-        __version__ = f.readline()  # read first line of the file
+        __version__ = f.readline()
 except FileNotFoundError:
+    # backup solution - read the version from git
     from pytripgui.version import git_version
     __version__ = git_version()
