0cbc227cdfaf8d833b86f3331aa03d0724cefcbc
Leszek Grzanka <grzanka@agh.edu.pl>
2020-02-06 16:30:51 +0100

    missing import added

--- pytripgui/__init__.py
+++ pytripgui/__init__.py
@@ -1,6 +1,7 @@
 try:
     # find specification of the pytripgui module
     import importlib
+    import os
     spec = importlib.util.find_spec('pytripgui')
     init_location = spec.origin  # location of __init__.py file (the one you see now) in the filesystem
     version_file = os.path.join(os.path.dirname(init_location), 'VERSION')  # VERSION should sit next to __init__.py in the directory structure
