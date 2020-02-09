31a9798a92799c49350fa7e92e28883dd7473c28
ljelen <47323191+ljelen@users.noreply.github.com>
2020-02-09 13:39:42 +0100

    Kernel dialog unit test (#271)
    * init kernel dialog unit test
    
    * change
    
    * Gather tests into one class

---
+++ tests/test_kernel_dialog.py
@@ -0,0 +1,51 @@
+import logging
+from pytestqt.qt_compat import qt_api
+
+from pytripgui.kernel_vc import KernelController
+from pytripgui.kernel_vc import KernelQtView
+from pytrip.tripexecuter import KernelModel
+from pytrip.tripexecuter import Projectile
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
+
+
+class TestKernelDialog:
+    kernels = []
+    # Kernel 1
+    ker = KernelModel()
+    ker.projectile = Projectile("C")
+    kernels.append(ker)
+    # Kernel 2
+    ker = KernelModel()
+    ker.projectile = Projectile("H")
+    kernels.append(ker)
+
+    @staticmethod
+    def test_basics(qtbot):
+        assert qt_api.QApplication.instance() is not None
+
+        view = KernelQtView()
+        controller = KernelController(TestKernelDialog.kernels, view)
+        controller.set_view_from_model()
+
+        qtbot.addWidget(view)
+        view.ui.show()
+        assert view.ui.isVisible()
+
+        # selecting kernel to edit
+        current_kernel_index = 1
+        view.ui.beamKernel_comboBox.setCurrentIndex(current_kernel_index)
+        assert view.projectile_symbol == TestKernelDialog.kernels[current_kernel_index].projectile.iupac
+
+        # setting new name
+        new_kernel_name = "Proton"
+        view.kernel_name = new_kernel_name
+
+        # clicking "OK"
+        view.ui.accept_buttonBox.accepted.emit()
+        assert not view.ui.isVisible()
+        assert controller.user_clicked_save
+
+        # checking if new name was saved
+        assert TestKernelDialog.kernels[current_kernel_index].name == new_kernel_name
