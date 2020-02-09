1051578fe9ed3933951a58186e64c1b446fb112d
Lukasz Jelen <lukasz.jelen@ifj.edu.pl>
2020-02-09 13:02:38 +0100

    Gather tests into one class

--- tests/test_kernel_dialog.py
+++ tests/test_kernel_dialog.py
@@ -9,41 +9,43 @@ from pytrip.tripexecuter import Projectile
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG)
 
-kernels = []
-# Kernel 1
-ker = KernelModel()
-ker.projectile = Projectile("C")
-kernels.append(ker)
-# Kernel 2
-ker = KernelModel()
-ker.projectile = Projectile("H")
-kernels.append(ker)
 
+class TestKernelDialog:
+    kernels = []
+    # Kernel 1
+    ker = KernelModel()
+    ker.projectile = Projectile("C")
+    kernels.append(ker)
+    # Kernel 2
+    ker = KernelModel()
+    ker.projectile = Projectile("H")
+    kernels.append(ker)
 
-def test_basics(qtbot):
-    assert qt_api.QApplication.instance() is not None
+    @staticmethod
+    def test_basics(qtbot):
+        assert qt_api.QApplication.instance() is not None
 
-    view = KernelQtView()
-    controller = KernelController(kernels, view)
-    controller.set_view_from_model()
+        view = KernelQtView()
+        controller = KernelController(TestKernelDialog.kernels, view)
+        controller.set_view_from_model()
 
-    qtbot.addWidget(view)
-    view.ui.show()
-    assert view.ui.isVisible()
+        qtbot.addWidget(view)
+        view.ui.show()
+        assert view.ui.isVisible()
 
-    # selecting kernel to edit
-    current_kernel_index = 1
-    view.ui.beamKernel_comboBox.setCurrentIndex(current_kernel_index)
-    assert view.projectile_symbol == kernels[current_kernel_index].projectile.iupac
+        # selecting kernel to edit
+        current_kernel_index = 1
+        view.ui.beamKernel_comboBox.setCurrentIndex(current_kernel_index)
+        assert view.projectile_symbol == TestKernelDialog.kernels[current_kernel_index].projectile.iupac
 
-    # setting new name
-    new_kernel_name = "Proton"
-    view.kernel_name = new_kernel_name
+        # setting new name
+        new_kernel_name = "Proton"
+        view.kernel_name = new_kernel_name
 
-    # clicking "OK"
-    view.ui.accept_buttonBox.accepted.emit()
-    assert not view.ui.isVisible()
-    assert controller.user_clicked_save
+        # clicking "OK"
+        view.ui.accept_buttonBox.accepted.emit()
+        assert not view.ui.isVisible()
+        assert controller.user_clicked_save
 
-    # checking if new name was saved
-    assert kernels[current_kernel_index].name == new_kernel_name
+        # checking if new name was saved
+        assert TestKernelDialog.kernels[current_kernel_index].name == new_kernel_name
