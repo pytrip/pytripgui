91cd6a754220632714972f52b4005dfe61da116b
Lukasz Jelen <lukasz.jelen@ifj.edu.pl>
2020-02-24 07:06:27 +0100

    widged to view test

---
+++ tests/treeView_test.py
@@ -0,0 +1,34 @@
+import sys
+import logging
+from PyQt5.QtWidgets import QApplication
+
+from pytripgui.view.qt_gui import UiMainWindow
+from pytripgui.treewidget_vc.treewidget_view import TreeWidgetView
+from pytripgui.treewidget_vc.treewidget_cont import TreeWidgetController
+from pytripgui.plan_executor.patient_model import PatientModel
+from pytripgui.treewidget_vc.patient_tree_model import PatientTreeModel
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
+
+
+app = QApplication(sys.argv)
+widget = UiMainWindow()
+widget.show()
+
+
+patient_list = list()
+patient_list.append(PatientModel())
+patient_list.append(PatientModel())
+patient_list.append(PatientModel())
+patient_list[0].name = "Patient nr 0"
+patient_list[1].name = "Another one"
+patient_list[2].name = "Third Patient"
+
+treeView = TreeWidgetView(widget.patient_treeView)
+model = PatientTreeModel(patient_list)
+widget.patient_treeView.setModel(model)
+
+treeControler = TreeWidgetController(patient_list, treeView)
+
+app.exec_()
