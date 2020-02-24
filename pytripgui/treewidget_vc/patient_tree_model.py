ed5192ad485d6d8978ee553d62eca68ef7a2193d
JelenL <lukasz.jelen@loycon.pl>
2020-02-24 23:37:02 +0100

    treeModel cleaning

--- pytripgui/treewidget_vc/patient_tree_model.py
+++ pytripgui/treewidget_vc/patient_tree_model.py
@@ -1,24 +1,44 @@
-from PyQt5.QtCore import QVariant, QModelIndex, Qt
-from PyQt5.QtCore import QAbstractItemModel
+from enum import Enum
 import logging
 
+from PyQt5.QtCore import QVariant, QModelIndex, Qt
+from PyQt5.QtCore import QAbstractItemModel
+
 logger = logging.getLogger(__name__)
+
+
+class PatientItemType(Enum):
+    ROOT = 0
+    PATIENT = 1
+    PLAN = 2
+    FIELD = 3
 
 
 class PatientItem:
     def __init__(self, data, parent=None):
         self._data = data
         self._parent_item = parent
+        self._internal_pointer = None   # to prevent garbage collector deleting PatientItem
+
+        if parent is None:
+            self._item_type = PatientItemType.ROOT
+
+    def row_count(self):
+        return len(self._data)
+
+    def has_children(self):
+        if self._item_type == PatientItemType.ROOT:
+            return len(self._data) > 0
 
     def has_index(self, p_int):
-        if p_int < len(self._data):
-            return True
+        if self._item_type == PatientItemType.ROOT:
+            if p_int < len(self._data):
+                return True
         return False
 
     def row(self):
         if self._parent_item:
             return self._parent_item.child_items.index(self._data)
-
         return 0
 
     def index(self, p_int, p_int_1, obj):
@@ -26,21 +46,28 @@ class PatientItem:
                 p_int_1 != 0:   # only one column is supported
             return QModelIndex()
 
-        if self._parent_item is None:
+        if self._item_type == PatientItemType.ROOT:
             patient_item = PatientItem(self._data[p_int], self)
+            patient_item._internal_pointer = patient_item
             index = obj.createIndex(p_int, p_int_1, patient_item)
             return index
 
         return QModelIndex()
 
     def parent(self):
-        if self._parent_item is None:
+        if self._item_type == PatientItemType.ROOT:
             return QModelIndex()
         return self._parent_item
 
-    @property
     def child_items(self):
         return self._data
+
+    @property
+    def name(self):
+        if isinstance(self._data, list):
+            return "Patient list"
+        else:
+            return self._data.name
 
 
 class PatientTreeModel(QAbstractItemModel):
@@ -51,26 +78,25 @@ class PatientTreeModel(QAbstractItemModel):
     def headerData(self, p_int, Qt_Orientation, role=None):
         if role == Qt.DisplayRole:
             return QVariant("Patients: ")
+        return QVariant()
 
     def columnCount(self, parent=None, *args, **kwargs):
         logger.debug("columnCount()")
         return 1
 
     def rowCount(self, parent=None, *args, **kwargs):
-        logger.debug("roeCount()")
-        return 2
+        logger.debug("rowCount()")
+        return self._root_item.row_count()
 
     def hasChildren(self, parent=None, *args, **kwargs):
         logger.debug("hasChildren()")
         if not parent.isValid():
-            print("It hasn't got a parent, means this is root item")
-            return True
+            return self._root_item.has_children()
 
-        print("It has got a parent")
-        return True
+        return False
 
     def index(self, p_int, p_int_1, parent=None, *args, **kwargs):
-        logger.debug("index(){}{}".format(p_int, p_int_1))
+        logger.debug("index() {} {}".format(p_int, p_int_1))
 
         if not self.hasIndex(p_int, p_int_1, parent) or \
                 parent is None:
@@ -95,14 +121,17 @@ class PatientTreeModel(QAbstractItemModel):
         logger.debug("data()")
 
         if role == Qt.DisplayRole:
-            return "Display"
+            return q_model_index.internalPointer().name
 
         if role == Qt.UserRole:
             return "user"
 
     def parent(self, q_model_index=None):
-    #     logger.debug("parent()")
-    #     if not q_model_index.isValid():
+        logger.debug("parent()")
+        if self._root_item == q_model_index:
+            logger.debug("q_model_index is invalid")
+            return QModelIndex()
+
         return QModelIndex()
     #
     #     if q_model_index.internalPointer() is None:
