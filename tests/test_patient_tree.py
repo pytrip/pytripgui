852d12b8a27386e2f9d20cad525daac6acaa9917
ljelen <lukasz.jelen@ifj.edu.pl>
2020-07-09 00:50:51 +0200

    add patient tree unit test

---
+++ tests/test_patient_tree.py
@@ -0,0 +1,21 @@
+import logging
+from pytestqt.qt_compat import qt_api
+from PyQt5.QtWidgets import QMainWindow
+
+from pytripgui.app_logic.patient_tree import PatientTree
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
+
+
+def test_basics(qtbot):
+    assert qt_api.QApplication.instance() is not None
+    main_window = QMainWindow()
+    patient_tree = PatientTree()
+    patient_tree.show(main_window)
+    main_window.show()
+
+    qtbot.addWidget(main_window)
+    main_window.show()
+
+    assert main_window.isVisible()
