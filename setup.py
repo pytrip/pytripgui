987ff2350245cfe3caaa6e4b6462e8e0c8e047ef
Leszek Grzanka <leszek.grzanka@gmail.com>
2020-09-04 21:44:21 +0200

    fixed OSI classifiers

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ setuptools.setup(
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8'
     ],
     package_data={'pytripgui': ['res/*', 'view/*.ui', 'VERSION']},
