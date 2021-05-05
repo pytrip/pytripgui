2295301dcc035f815231aea276aa504438f401ec
Joanna Fortuna <yoanka97@gmail.com>
2021-05-05 22:52:03 +0200

    add empty patient; general small fixes

--- pytripgui/plan_executor/patient_model.py
+++ pytripgui/plan_executor/patient_model.py
@@ -37,3 +37,27 @@ class PatientModel:
         if 'rtss' in self.dcm:
             self.vdx = pt.VdxCube(self.ctx)
             self.vdx.read_dicom(self.dcm)
+
+    def init_with_empty_cube(self, cube_params=None):
+        if cube_params is None:
+            cube_params = [255, 100, 100, 100, 1, 5]
+        value, dimx, dimy, dimz, pixel_size, slice_distance = cube_params
+        cube = pt.Cube()
+        cube.create_empty_cube(value, dimx, dimy, dimz, pixel_size, slice_distance)
+        self.ctx = pt.CtxCube(cube)
+        self.ctx.basename = "arek"
+        sphere_params = ["asia", [50, 50, 50], 5]
+
+        self.vdx = pt.VdxCube(self.ctx)
+        sphere = pt.vdx.create_sphere(self.ctx, *sphere_params)
+        # sphere.color = [255, 0, 0]
+        # print(sphere)
+        # print('#{:02x}{:02x}{:02x}'.format(sphere.color[0], sphere.color[1], sphere.color[2]))
+        self.vdx.add_voi(sphere)
+        self.vdx.basename = "arek"
+        # print(self.vdx.vois)
+        self.name = "arek"
+
+    def insert_empty_sphere(self, sphere_params):
+        pass
+
