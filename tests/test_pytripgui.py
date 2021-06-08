4575a0ecc2ff258e2f12abb7b9fa966b7b3a975e
Leszek Grzanka <leszek.grzanka@gmail.com>
2021-06-08 22:16:39 +0200

    Update test_pytripgui.py

--- tests/test_pytripgui.py
+++ tests/test_pytripgui.py
@@ -9,7 +9,7 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG)
 
 
-@pytest.mark.skipif((sys.version_info[0] == 3) and (sys.version_info[1] == 7), 
+@pytest.mark.skipif((sys.version_info[0] == 3) and (sys.version_info[1] == 7),
                     reason="fails on python 3.7 for unknown reasons")
 def test_basics(qtbot):
     widget = UiMainWindow()
