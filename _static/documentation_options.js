84bbbb85c308d0345d0c5912773c3b452433b05c
grzanka <grzanka@users.noreply.github.com>
2025-12-11 13:03:48 +0000

    deploy: a55edc1b838157cbb6d31135881bc4fd649cad67

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.5.0.post15+g922bc93c1',
+    VERSION: '1.5.0.post16+ga55edc1b8',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
